20172ff0407f4fb7fc016819b852518be8aff3d3
Aron <aron.kubat@gmail.com>
2026-05-22 00:59:06 -0400

    finish movement

---
+++ Assets/Scripts/MouseLockControl.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MouseLockControl : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = (Cursor.lockState == CursorLockMode.Locked) ? CursorLockMode.None : CursorLockMode.Locked;
+            Cursor.visible = (Cursor.lockState != CursorLockMode.Locked);
+        }
+    }
+
+}
